2b4d5caddf41ed67d5864e590a57dee2d3ee2bce
PlashSpeed-Aiman <maiman.ar@live.iium.edu.my>
2023-01-31 21:51:05 +0800

    Added Changes to Code

--- app/src/main/java/com/example/ict652/Adapters/MemberAdapter.java
+++ app/src/main/java/com/example/ict652/Adapters/MemberAdapter.java
@@ -1,6 +1,79 @@
 package com.example.ict652.Adapters;
 
-public class MemberAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ict652.R;
+import com.example.ict652.interfaces.RecyclerViewClickListener;
+import com.example.ict652.model.MemberModel;
+
+import java.util.ArrayList;
+
+public class MemberAdapter extends RecyclerView.Adapter<MemberAdapter.MyViewHolder> {
+    ArrayList<MemberModel> arrayList;
+    Context context;
+    //    final private RecyclerViewClickListener clickListener;
+    public MemberAdapter(ArrayList<MemberModel> arrayList) {
+        this.arrayList = arrayList;
+        this.context = context;
+//        this.clickListener = clickListener;
+    }
+    @NonNull
+    @Override
+    public MemberAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(
+                        R.layout.member_list_item,
+                        parent, false);
+
+        final MemberAdapter.MyViewHolder myViewHolder = new MemberAdapter.MyViewHolder(view);
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MemberAdapter.MyViewHolder holder, int position) {
+        final String name = arrayList.get(position).getName();
+        final String role = arrayList.get(position).getRole();
+        holder.name_view.setText(name);
+        holder.role_view.setText(role);
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrayList.size();
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView name_view;
+        TextView role_view;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name_view = itemView.findViewById(R.id.member_name_view);
+            role_view = itemView.findViewById(R.id.member_role_view);
 
 
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    clickListener.onItemClick(getAdapterPosition());
+//                }
+//            });
+//
+//            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+//                @Override
+//                public boolean onLongClick(View v) {
+//                    clickListener.onLongItemClick(getAdapterPosition());
+//                    return true;
+//                }
+//            });
+
+
+        }
+    }
 }
--- app/src/main/java/com/example/ict652/Adapters/TodoListAdapter.java
+++ app/src/main/java/com/example/ict652/Adapters/TodoListAdapter.java
@@ -4,17 +4,18 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ict652.R;
 import com.example.ict652.interfaces.RecyclerViewClickListener;
-import com.example.ict652.model.MemberModel;
 import com.example.ict652.model.TodoModel;
 
 import java.util.ArrayList;
@@ -24,6 +25,10 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
     ArrayList<TodoModel> arrayList;
     Context context;
     final private RecyclerViewClickListener clickListener;
+    private RecyclerView.RecycledViewPool
+            viewPool
+            = new RecyclerView
+            .RecycledViewPool();
     public TodoListAdapter(Context context, ArrayList<TodoModel> arrayList, RecyclerViewClickListener clickListener) {
         this.arrayList = arrayList;
         this.context = context;
@@ -47,8 +52,11 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
             public void onClick(View v) {
                 if(myViewHolder.accordian_body.getVisibility() == View.VISIBLE) {
                     myViewHolder.accordian_body.setVisibility(View.GONE);
+                    myViewHolder.childRecyclerView.setVisibility(View.GONE);
                 } else {
                     myViewHolder.accordian_body.setVisibility(View.VISIBLE);
+                    myViewHolder.childRecyclerView.setVisibility(View.VISIBLE);
+
                 }
             }
         });
@@ -67,7 +75,58 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
         if(!description.equals("")) {
             holder.descriptionTv.setText(description);
         }
+        TodoModel parentItem
+                = arrayList.get(position);
 
+        // For the created instance,
+        // get the title and set it
+        // as the text for the TextView
+//        parentViewHolder
+//                .ParentItemTitle
+//                .setText(parentItem.getParentItemTitle());
+
+        // Create a layout manager
+        // to assign a layout
+        // to the RecyclerView.
+
+        // Here we have assigned the layout
+        // as LinearLayout with vertical orientation
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(
+                holder
+                        .childRecyclerView
+                        .getContext(),
+                LinearLayoutManager.VERTICAL,
+                false);
+
+        // Since this is a nested layout, so
+        // to define how many child items
+        // should be prefetched when the
+        // child RecyclerView is nested
+        // inside the parent RecyclerView,
+        // we use the following method
+        layoutManager
+                .setInitialPrefetchItemCount(
+                        parentItem
+                                .getMemberModelArrayList()
+                                .size());
+
+        // Create an instance of the child
+        // item view adapter and set its
+        // adapter, layout manager and RecyclerViewPool
+        MemberAdapter childItemAdapter
+                = new MemberAdapter(
+                parentItem
+                        .getMemberModelArrayList());
+        holder
+                .childRecyclerView
+                .setLayoutManager(layoutManager);
+        holder
+                .childRecyclerView
+                .setAdapter(childItemAdapter);
+        holder
+                .childRecyclerView
+                .setRecycledViewPool(viewPool);
     }
 
     @Override
@@ -80,7 +139,12 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
         CardView accordian_title;
         TextView titleTv, descriptionTv, name, role;
         RelativeLayout accordian_body;
-        ImageView arrow, editBtn, deleteBtn, doneBtn, addMember;
+        ImageView arrow;
+        ImageView editBtn;
+        ImageView deleteBtn;
+        ImageView doneBtn;
+        ImageView addMember;
+        private RecyclerView childRecyclerView;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -93,8 +157,8 @@ public class TodoListAdapter extends RecyclerView.Adapter<TodoListAdapter.MyView
             editBtn = (ImageView) itemView.findViewById(R.id.editBtn);
             deleteBtn = (ImageView) itemView.findViewById(R.id.deleteBtn);
             doneBtn = (ImageView) itemView.findViewById(R.id.doneBtn);
-            addMember = (ImageView) itemView.findViewById(R.id.addMember);
-
+            addMember =  itemView.findViewById(R.id.addMember);
+            childRecyclerView = itemView.findViewById(R.id.recycler_view_member);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
